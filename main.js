180994e6dc2bf9fea15adf4ee239c789d15ea5c5
rramtcr <https://github.com/rramtcr/app-ip_address.git>
2020-05-10 16:58:07 +0000

    Lab 4

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ class IpAddress {
     // For more information, consult the Log Class guide on the Itential
     // Developer Hub https://developer.itential.io/ located
     // under Documentation -> Developer Guides -> Log Class Guide
-   // log.info('Starting the IpAddress product.');
+    //log.info('Starting the IpAddress product.');
   }
  
  getFirstIpAddress(cidrStr, callback) {
@@ -107,3 +107,4 @@ class IpAddress {
 }
  
 module.exports = new IpAddress;
+
